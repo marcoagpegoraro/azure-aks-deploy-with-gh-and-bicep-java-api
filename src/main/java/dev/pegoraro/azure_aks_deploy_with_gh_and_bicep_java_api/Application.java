d9007c52ad94a2c7ee336e74ddc0099a41126c3e
Marco Pegoraro <pegoraro@Marcos-MacBook-Air.local>
2026-03-16 11:41:02 +0100

    initial commit

---
+++ src/main/java/dev/pegoraro/azure_aks_deploy_with_gh_and_bicep_java_api/Application.java
@@ -0,0 +1,13 @@
+package dev.pegoraro.azure_aks_deploy_with_gh_and_bicep_java_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pegoraro/azure_aks_deploy_with_gh_and_bicep_java_api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pegoraro.azure_aks_deploy_with_gh_and_bicep_java_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
